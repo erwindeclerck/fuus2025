73a436a7d6054d3538d43db80cdf6f8cdf51f129
Erwin De Clerck <erwin.de.clerck@gmail.com>
2025-01-09 02:01:34 +0100

    initial commit

---
+++ web/themes/custom/fuus2025_sass/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.fuus2025_sass = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
