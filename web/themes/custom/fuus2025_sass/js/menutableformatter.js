51e6b5e5d7d10cd4a69c28598960a8eb0e5229ed
Erwin De Clerck <erwin.de.clerck@gmail.com>
2025-03-20 08:56:32 +0100

    aanpassing js voor menu

--- web/themes/custom/fuus2025_sass/js/menutableformatter.js
+++ web/themes/custom/fuus2025_sass/js/menutableformatter.js
@@ -11,8 +11,12 @@
     attach: function (context, settings) {
 
 
-      var nid = settings.path.currentPath.match(/node\/(\d+)/)[1];
-      var tfid = 'table#tablefield-node-'+nid+'-field_menu-0';
+      // var nid = settings.path.currentPath.match(/node\/(\d+)/)[1];
+      // var tfid = 'table#tablefield-node-'+nid+'-field_menu-0';
+      var tfid = $('[id^=tablefield-node-]').attr('id');
+      if(tfid){
+
+      var nid = tfid.match(/tablefield-node-(\d+)-field_menu-0/)[1];
       function newtable(parentid, thisid, header, color) {
         var html = "";
         html += "<table id=T" + thisid + " class='table table-striped table-hover table-sm table-responsive caption-top' style='margin-bottom:2em;'>";
@@ -26,7 +30,7 @@
       var splitIndices = [];
       var titles = [];
       var colors = [];
-      var rows = $(tfid + ' tbody').children();
+      var rows = $("#"+tfid + ' tbody').children();
 
       rows.each(function (i, row) {
         var trigger = $(this).children('td').eq(0).text();
@@ -50,16 +54,18 @@
         }
       });
 
-      $(tfid).remove();
+      $("#"+tfid).remove();
 
       $(function () {
           for (var i = 0; i < splitIndices.length; i++) {
-            var thistable = newtable('#tablefield-wrapper-node-'+nid+'-field_menu-0', i, titles[i], colors[i]);
+           var thistable = newtable('#tablefield-wrapper-node-'+nid+'-field_menu-0', i, titles[i], colors[i]);
+        //    var thistable = newtable(tfid, i, titles[i], colors[i]);
             rows.slice(splitIndices[i] + 1, splitIndices[i + 1]).appendTo(thistable);
 
           }
         }
       );
+      }
 
 
 
